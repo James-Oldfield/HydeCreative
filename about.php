b6923ef28990f851d7dd252b94421932d4b81f43
James Oldfield <hi@james-oldfield.co>
2014-11-11 22:44:21 +0000

    Added about page template

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: About template
+*/ ?>
+
+<?php get_header(); ?>
+
+<div class="about col-lg-12">
+<p>Hyde is a new creative agency that focuses on helping creatives create. From photographers to fashion designers, Hyde helps them to push their ideas out into the world.</p>
+ 
+<p>We also have a passion for pushing the idea of what creativity and publishing can be. Within the next 12 months we have an array of agency let publications planned. Exploring sex, gender and creativity are but some of the subjects we are going to explore.</p>
+ 
+<p>Watch this space.</p>
+ 
+<p>For more information contact: <a href="mailto:info@hydecreative.co.uk">info@hydecreative.co.uk</a> </p>
+</div>
+
+<?php get_footer(); ?>
