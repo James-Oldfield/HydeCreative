c1d74100e4a3986b762ea23488e46e8087160c70
James Oldfield <hi@james-oldfield.co>
2014-11-12 10:11:05 +0000

    Added monkey typewriter

--- comingSoon.php
+++ comingSoon.php
@@ -5,10 +5,13 @@ Template Name: comingSoon template
 
 <?php get_header(); ?>
 
-<div class="about col-lg-12">
-<p>There are lots of monkeys on typewriters creating content for this site as we speak.</p>
- 
-<p class="watch">Watch this space.</p>
+<div class="about">
+	<div class="col-left col-xs-12 col-md-6">
+		<img class="img-responsive monkey" src="<?php echo get_stylesheet_directory_uri(); ?>/img/monkey.svg" width="100%" height="auto"/>
+	</div>
+
+	<div id="type" class="type"></div>
+	<div class="cursor">|</div>
 </div>
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <div class="container main text-center">
 	<div class="landing-page">
-            <div class="col-xs-12">
-            </div>      
+      <div class="col-xs-12">
+      </div>      
 	</div>        
 </div>
 
